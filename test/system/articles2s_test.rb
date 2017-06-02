2bdb70f99150636d324a7b22cbb7aaeb76630844
nakamutaku <17hotoda@nao.net.it-chiba.ac.jp>
2017-06-02 04:23:45 -0700

    add articles

---
+++ test/system/articles2s_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Articles2sTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit articles2s_url
+  #
+  #   assert_selector "h1", text: "Articles2"
+  # end
+end
